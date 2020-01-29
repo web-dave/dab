e8532711b8ee9aafe2be1e6741967d7b2ace124d
Dave <dave@webdave.de>
2020-01-29 14:16:16 +0100

    details

--- src/app/books/book-details/book-details.component.ts
+++ src/app/books/book-details/book-details.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { BookService } from '../shared/book.service';
+import { IBook } from '../shared/ibook';
 
 @Component({
   selector: 'app-book-details',
@@ -6,10 +9,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./book-details.component.scss']
 })
 export class BookDetailsComponent implements OnInit {
-
-  constructor() { }
+  book: IBook;
+  constructor(private route: ActivatedRoute, private service: BookService) {}
 
   ngOnInit() {
+    this.route.params.subscribe(params => {
+      this.service.getBook(params.isbn).subscribe(b => (this.book = b));
+    });
   }
-
 }
--- src/app/books/shared/book.service.ts
+++ src/app/books/shared/book.service.ts
@@ -7,26 +7,13 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root'
 })
 export class BookService {
-  private books: IBook[] = [
-    {
-      title: 'Design Patterns',
-      subtitle: 'Elements of Reusable Object-Oriented Software'
-    },
-    {
-      title: 'REST und HTTP',
-      subtitle: 'Entwicklung und Integration nach dem Architekturstil des Web'
-    },
-    {
-      title: 'Eloquent JavaScript',
-      subtitle: 'A Modern Introduction to Programming'
-    }
-  ];
   url = 'http://localhost:4730/books/';
   constructor(private http: HttpClient) {}
-  getBooksSync(): IBook[] {
-    return this.books;
-  }
+
   getBooks(): Observable<IBook[]> {
     return this.http.get<IBook[]>(this.url);
+  }
+  getBook(isbn: string): Observable<IBook> {
+    return this.http.get<IBook>(this.url + isbn);
   }
 }
